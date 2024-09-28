bc41887c8f20f86b58c928d867202309f97d4a19
Vishnu N <101573989+Vishnu2426@users.noreply.github.com>
2024-09-28 15:03:53 +0530

    Update wsgi.py

--- Assignment/costproject/wsgi.py
+++ Assignment/costproject/wsgi.py
@@ -15,4 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'costproject.settings')
 
 application = get_wsgi_application()
-
+app= application
